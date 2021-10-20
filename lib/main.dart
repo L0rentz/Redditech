2bfa6734c60412e8efe12667e41d5c744d5d2839
Marton Szuts <marton.szuts@epitech.eu>
2021-10-20 17:38:30 +0200

    front and homepage

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,7 @@
-import 'dart:async';
-import 'dart:developer';
-import 'dart:io';
-
-import 'package:draw/draw.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_web_auth/flutter_web_auth.dart';
+import 'package:flutter_application_1/theme/redditech_colors.dart';
 
-import 'package:oauth2/oauth2.dart' as oauth2;
-import 'package:url_launcher/url_launcher.dart';
-import 'package:uuid/uuid.dart';
+import 'pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,81 +13,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Palette.redditOrange,
+        scaffoldBackgroundColor: Colors.grey.shade300,
+        textTheme: const TextTheme(
+            headline1: TextStyle(
+                fontFamily: 'Frankfurter', fontSize: 40, color: Colors.black),
+            button: TextStyle(
+                fontFamily: 'OpenSans', fontSize: 18, color: Colors.white)),
       ),
-      home: const MyHomePage(title: 'Redditech'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  Future<void> _test() async {
-    var uuid = const Uuid();
-    String userAgent = uuid.v1();
-
-    // Create a `Reddit` instance using a configuration file in the current
-    // directory. Unlike the web authentication example, a client secret does
-    // not need to be provided in the configuration file.
-    final reddit = Reddit.createInstalledFlowInstance(
-        userAgent: userAgent,
-        clientId: 'h-leSR3fD6gG3C6hL2mqBw',
-        tokenEndpoint:
-            Uri.parse('https://www.reddit.com/api/v1/authorize.compact'),
-        redirectUri: Uri.parse('redditech://callback'));
-
-    // Build the URL used for authentication. See `WebAuthenticator`
-    // documentation for parameters.
-    final auth_url = reddit.auth.url(['*'], userAgent, compactLogin: true);
-
-    print(auth_url);
-
-    final result = await FlutterWebAuth.authenticate(
-        url: auth_url.toString(), callbackUrlScheme: "redditech");
-
-    print(result);
-    final code = Uri.parse(result).queryParameters['code'];
-
-    await reddit.auth.authorize(code!);
-
-    print(await reddit.user.me());
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Test Authentification',
-            ),
-            Text(
-              'Auth',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _test,
-        tooltip: 'Reddit auth',
-        child: const Icon(Icons.add),
-      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const MyHomePage(title: "Redditech"),
+      },
     );
   }
 }
