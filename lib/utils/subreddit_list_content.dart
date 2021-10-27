0c4799c1e0fa35cb9414197fbf1d05cc22355803
Marton Szuts <marton.szuts@epitech.eu>
2021-10-27 19:35:25 +0200

    Posts half done

--- lib/utils/subreddit_list_content.dart
+++ lib/utils/subreddit_list_content.dart
@@ -22,35 +22,37 @@ class SubredditListContent extends StatelessWidget {
         Navigator.pushNamed(context, '/posts',
             arguments: SubredditPostArguments(element));
       },
-      child: SizedBox(
-        height: Global.screenHeight / 15,
-        child: Row(
-          children: [
-            const Spacer(),
-            if (iconUrl == null || iconUrl.toString() == "") ...[
-              CircleAvatar(
-                radius: Global.screenHeight / 40,
-                backgroundImage: const AssetImage("assets/placeholder.png"),
-              ),
-            ] else ...[
-              CircleAvatar(
-                radius: Global.screenHeight / 40,
-                backgroundImage: NetworkImage(iconUrl.toString()),
-              ),
-            ],
-            const Spacer(flex: 1),
-            SizedBox(
-              width: Global.screenWidth / 1.5,
-              child: Text(
-                title,
-                style: TextStyle(
-                  fontSize: Global.screenHeight / 48,
-                  fontFamily: "OpenSans",
+      child: Card(
+        child: SizedBox(
+          height: Global.screenHeight / 15,
+          child: Row(
+            children: [
+              const Spacer(),
+              if (iconUrl == null || iconUrl.toString() == "") ...[
+                CircleAvatar(
+                  radius: Global.screenHeight / 40,
+                  backgroundImage: const AssetImage("assets/placeholder.png"),
+                ),
+              ] else ...[
+                CircleAvatar(
+                  radius: Global.screenHeight / 40,
+                  backgroundImage: NetworkImage(iconUrl.toString()),
+                ),
+              ],
+              const Spacer(flex: 1),
+              SizedBox(
+                width: Global.screenWidth / 1.5,
+                child: Text(
+                  title,
+                  style: TextStyle(
+                    fontSize: Global.screenHeight / 48,
+                    fontFamily: "OpenSans",
+                  ),
                 ),
               ),
-            ),
-            const Spacer(flex: 6),
-          ],
+              const Spacer(flex: 6),
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/utils/subreddit_post_content.dart
@@ -0,0 +1,51 @@
+import 'package:draw/draw.dart';
+import 'package:flutter/material.dart';
+
+import '../global.dart';
+
+class PostContent extends StatefulWidget {
+  const PostContent({Key? key, required this.element}) : super(key: key);
+
+  final Submission element;
+  @override
+  _PostContentState createState() => _PostContentState();
+}
+
+class _PostContentState extends State<PostContent> {
+  String getPostAge(int time) {
+    String ret;
+    ret = time.toString() + "min";
+    if (time >= 60) {
+      time ~/= 60;
+      ret = time.toString() + "h";
+    } else {
+      return (ret);
+    }
+    if (time >= 24) {
+      time ~/= 24;
+      ret = time.toString() + "d";
+    }
+    if (time >= 365) {
+      int rett = time % 365;
+      time ~/= 365;
+      ret = time.toString() + "y " + rett.toString() + "d";
+    }
+    return (ret);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    int ass = widget.element.createdUtc.millisecondsSinceEpoch.toInt();
+    int redditAge = (DateTime.now().millisecondsSinceEpoch - ass) ~/ 60000;
+
+    return Card(
+      child: Column(
+        children: [
+          Text("Post from: " + widget.element.author),
+          Text("Post: " + widget.element.title),
+          Text(getPostAge(redditAge))
+        ],
+      ),
+    );
+  }
+}
