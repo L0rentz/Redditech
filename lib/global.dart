636c315840d38e8c6cf0b627111df6ede23ac4bd
Lorentz-Andrea Romeo <lorentz-andrea.romeo@epitech.eu>
2021-10-29 21:30:27 +0200

    global fix

--- lib/global.dart
+++ lib/global.dart
@@ -3,6 +3,7 @@ import 'package:draw/draw.dart';
 class Global {
   static late Reddit? reddit;
   static late Redditor? redditor;
+  static late Subreddit? subreddit;
   static late double screenHeight;
   static late double screenWidth;
   static late double minScreenSize;
